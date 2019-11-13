318b93b7e3fb26a4ddb094e366bc40ff06377b1c
Noah <ngonzalezs01@informatica.iesvalledeljerteplasencia.es>
2019-11-13 19:32:32 +0100

    Correcion de Errores
    Pase la interfaz creada a otra clase (PixelPaint)

--- src/Noah_Gonzalez_Sanz/Principal.java
+++ src/Noah_Gonzalez_Sanz/Principal.java
@@ -3,8 +3,11 @@ package Noah_Gonzalez_Sanz;
 public class Principal {
 	public static void main(String[] args) {
 		VentanaPrincipal marco = new VentanaPrincipal();
-		marco.añadirElementos();
+		marco.paint.añadirElementos();
+		marco.paint.iniciarlizarListener();
 		marco.visible();
+		
+		
 	}
 
 }
--- src/Noah_Gonzalez_Sanz/VentanaPrincipal.java
+++ src/Noah_Gonzalez_Sanz/VentanaPrincipal.java
@@ -1,146 +1,27 @@
 package Noah_Gonzalez_Sanz;
 
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
-import java.awt.Insets;
 
-import javax.swing.BorderFactory;
-import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
 
 public class VentanaPrincipal {
 	JFrame marco;
-	JPanel panelSelectorColor;
-	JPanel [][] lienzo;
-	JPanel panelPixel;
-	JButton botonNegro;
-	JButton botonAzul;
-	JButton botonVerde;
-	JButton botonRojo;
-	JLabel texto;
+	PixelPaint paint;
 
 	public VentanaPrincipal() {
 		marco = new JFrame("Pixel Paint");
+		paint = new PixelPaint();
 		marco.setBounds(0, 0, 900, 600);
-		marco.setLayout(new GridBagLayout());
+		marco.setLayout(new GridLayout(0,1));
+		marco.add(paint);
 		marco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
-	/**
-	 * Inicializamos los paneles
-	 */
-	public void añadirElementos() {
-		//Panel SeleccionColor
-		panelSelectorColor = new JPanel();
-		panelSelectorColor.setLayout(new GridBagLayout());
-		panelSelectorColor.setBorder(BorderFactory.createMatteBorder(20, 20, 20, 20, Color.CYAN));
-		añadirBotones();
-		
-		//Panel Pixel
-		panelPixel = new JPanel();
-		panelPixel.setLayout(new GridLayout(20, 20));
-		añadirLienzo();
-
-	}
-	/**
-	 * Añadimos al panelPixel la matriz de paneles
-	 */
-	public void añadirLienzo() {
-		GridBagConstraints setting = new GridBagConstraints();
-		lienzo = new JPanel[20][20];
-		for (int i = 0; i < lienzo.length; i++) {
-			for (int j = 0; j < lienzo[i].length; j++) {
-				lienzo[i][j] = new JPanel();
-				setting.gridx = i;
-				setting.gridy = j;
-				setting.ipadx = 45;
-				setting.ipady = 45;
-				setting.fill = GridBagConstraints.BOTH;
-				lienzo[i][j].setBorder(BorderFactory.createLineBorder(Color.BLACK));
-				panelPixel.add(lienzo[i][j], setting);
-			}
-		}
-		setting = new GridBagConstraints();
-		setting.weightx = 5;
-		setting.weighty = 5;
-		setting.fill = GridBagConstraints.BOTH;
-		marco.add(panelPixel, setting);
-		
-	}
-	/**
-	 * Añadimos al panelSelectorColor , los botones y el label
-	 */
-	public void añadirBotones() {
-		GridBagConstraints setting = new GridBagConstraints();
-		//Label
-		Font font = new Font("Agency FB", Font.BOLD, 20);
-		texto = new JLabel("Seleccion de Colores");
-		texto.setFont(font);
-		texto.setForeground(Color.DARK_GRAY);
-		setting.gridx = 0;
-		setting.gridy = 0;
-		panelSelectorColor.add(texto,setting);
-		
-	//**Botones
-		
-		//Negro
-		botonNegro = new JButton();
-		botonNegro.setBackground(Color.BLACK);
-		setting = new GridBagConstraints();
-		setting.gridx = 0;
-		setting.gridy = 2;
-		setting.ipady = 25;
-		setting.ipadx = 25;
-		setting.insets = new Insets(20, 20, 20, 20);
-		panelSelectorColor.add(botonNegro,setting);
-		
-		//Azul
-		botonAzul = new JButton();
-		botonAzul.setBackground(Color.BLUE);
-		setting = new GridBagConstraints();
-		setting.gridx = 0;
-		setting.gridy = 3;
-		setting.ipady = 25;
-		setting.ipadx = 25;
-		panelSelectorColor.add(botonAzul,setting);
-		
-		//Verde
-		botonVerde = new JButton();
-		botonVerde.setBackground(Color.GREEN);
-		setting = new GridBagConstraints();
-		setting.gridx = 0;
-		setting.gridy = 4;
-		setting.ipady = 25;
-		setting.ipadx = 25;
-		setting.insets = new Insets(20, 20, 20, 20);
-		panelSelectorColor.add(botonVerde,setting);
-		
-		//Rojo
-		botonRojo = new JButton();
-		botonRojo.setBackground(Color.RED);
-		setting = new GridBagConstraints();
-		setting.gridx = 0;
-		setting.gridy = 5;
-		setting.ipady = 25;
-		setting.ipadx = 25;
-		panelSelectorColor.add(botonRojo,setting);
-		
-		//Añadir PanelSelectorColor
-		setting = new GridBagConstraints();
-		setting.weightx = 1;
-		setting.weighty = 1;
-		setting.fill = GridBagConstraints.BOTH;
-		marco.add(panelSelectorColor,setting);
-			
-	}
-
+	
 	public void visible() {
 		marco.setVisible(true);
 	}
+
+	
 
 }
