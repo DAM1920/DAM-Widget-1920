ec65d22e1accec59e317190d55c70de98741e065
Alba Vega Calzado <avegac01@informatica.iesvalledeljerteplasencia.es>
2019-11-13 18:54:54 +0100

    commit prueba

--- src/Vega_Calzado_Alba/VentanaPrincipal.java
+++ src/Vega_Calzado_Alba/VentanaPrincipal.java
@@ -1,5 +1,7 @@
 package Vega_Calzado_Alba;
 
+import java.awt.GridBagLayout;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JTextField;
@@ -8,10 +10,11 @@ public class VentanaPrincipal {
 	JFrame ventana;
 	JTextField buscadorRuta;
 	JButton botonBuscar;
+	Galeria galeria;
 	
 	public VentanaPrincipal() {
 		ventana=new JFrame();
-		ventana.setBounds();
+		ventana.setBounds(100, 100, 350, 250);
 		ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 	
@@ -22,7 +25,7 @@ public class VentanaPrincipal {
 	}
 	
 	public void inicializarComponentes() {
-		ventana.setLayout();
+		ventana.setLayout(new GridBagLayout());
 		
 		//Inicialización del JTextField
 		buscadorRuta = new JTextField();
