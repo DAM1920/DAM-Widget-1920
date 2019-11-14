372f227c4e27cc57ac9052b41d7202423b9c886b
PedroLuis <prodriguezp01@informatica.iesvalledeljerteplasencia.es>
2019-11-14 23:26:17 +0100

    Ultimos detalles

--- src/PedroLuis_Rodriguez_Perez/Formulario.java
+++ src/PedroLuis_Rodriguez_Perez/Formulario.java
@@ -27,55 +27,157 @@ import javax.swing.JTabbedPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.table.DefaultTableModel;
-
-public class Formulario extends JPanel {
-
-	
-	JPanel formulario;
-	JPanel visor;
-
-	JTabbedPane panelTabbed;
-
-	JLabel[] etiquetas;
-	JTextField[] cuadrosDeTexto;
-
-	JTable tabla;
-	DefaultTableModel model = new DefaultTableModel();
-
+/**
+ * Clase formulario, donde tendremos un tabbedPane donde lo dividiremos en un panel formulario y un panel visor de ficheros CSV.
+ * @author Pedro Luis
+ *
+ */
+public class Formulario extends JPanel {	
+	JPanel formulario; //Panel formulario.
+	JLabel[] etiquetas; //JLabels del formulario.
+	JTextField[] cuadrosDeTexto;//JText del formulario.
 	JButton almacenar;
-	File ficheroCSV;
-	ArrayList<String> atributos;
+	
+	JPanel visor; //Panel visor de fichero.
+	JTable tabla; //Tabla donde guardo los datos.
+	DefaultTableModel model = new DefaultTableModel();
+	
+	JTabbedPane panelTabbed; //Contenedor donde meto el formulario y el visor
+	
+	File ficheroCSV; //Fichero CSV con el que trabajo.
+	ArrayList<String> atributos;//Atributos del formulario
 
-	public Formulario(File ficheroCSV) {
-		
+	public Formulario(File ficheroCSV) {		
+		atributos = new ArrayList<String>();
 		this.setLayout(new GridLayout());
 		formulario = new JPanel();
-		formulario.setLayout(new GridBagLayout());
 		visor = new JPanel();
-		visor.setLayout(new GridLayout(1, 1));
-		this.ficheroCSV = ficheroCSV;
+		formulario.setLayout(new GridBagLayout());
 		tabla = new JTable(model);
-		atributos = new ArrayList<String>();
+		almacenar = new JButton("Almacenar");		
+		this.ficheroCSV = ficheroCSV;		
+		visor.setLayout(new GridLayout(1, 1));						
 		volcarAtributos();
-
-		for (int i = 0; i < atributos.size(); i++) {
-			model.addColumn(atributos.get(i));
-		}
+		aniadirColumas();
 		aniadirValores();
-
-		almacenar = new JButton("Almacenar");
-
 		etiquetas = new JLabel[atributos.size()];
 		cuadrosDeTexto = new JTextField[atributos.size()];
+		for (int i = 0; i < atributos.size(); i++) {
+			etiquetas[i]=new JLabel(atributos.get(i));
+			cuadrosDeTexto[i]=new JTextField();
+		}
+		panelTabbed = new JTabbedPane();
+		inicializarComponentes();
+	}
+	
+
+	private void inicializarComponentes() {
+		GridBagConstraints settings = new GridBagConstraints();
+
+		// Formulario
+		//formulario.setBorder(BorderFactory.createTitledBorder("Formulario"));
+		formulario.setBorder(BorderFactory.createMatteBorder(20, 5, 5, 5, new Color(51, 102, 153)));
+		formulario.setBackground(new Color(236, 242, 249));
+
+		panelTabbed.addTab("Formulario", new JScrollPane(formulario));
+
+		// Visor
+		visor.setBorder(BorderFactory.createTitledBorder("Visor"));
+		visor.setBackground(new Color(216, 229, 237));
+		tabla.setBackground(new Color(216, 229, 237));
+
+		visor.add(new JScrollPane(tabla));
+		
+		settings.weightx = 1;
+		settings.weighty = 1;
+		settings.fill = GridBagConstraints.BOTH;
+		panelTabbed.addTab("Visor del Fichero", new JScrollPane(visor));
+
+		this.add(panelTabbed);
+		
+		
+		
+		//Etiquetas y los cuadros de Texto
+
+		settings = new GridBagConstraints();
+		settings.insets = new Insets(0, 0, 40, 0);
+		//settings.fill = GridBagConstraints.BOTH;
 
 		for (int i = 0; i < atributos.size(); i++) {
-			etiquetas[i] = new JLabel(atributos.get(i));
-			cuadrosDeTexto[i] = new JTextField();
+			settings.gridx = 0;
+			settings.gridy = i;
+			settings.weightx = 1;
+			settings.ipadx = 100;
+			etiquetas[i].setForeground(new Color(136, 61, 29));
+			etiquetas[i].setFont(etiquetas[i].getFont().deriveFont(Font.BOLD, 14f));
+			formulario.add(etiquetas[i], settings);
+
+			settings.gridx = 1;
+			settings.gridy = i;
+			settings.weightx = 1;
+			formulario.add(cuadrosDeTexto[i], settings);
 		}
 
-		panelTabbed = new JTabbedPane();
+		// Boton almacenar
 
-		inicializarComponentes();
+		settings = new GridBagConstraints();
+
+		settings.gridy = atributos.size();
+		settings.insets = new Insets(50, 0, 0, 0);
+		settings.gridwidth = 2;
+		formulario.add(almacenar, settings);
+
+		almacenar.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+
+				String values[] = new String[cuadrosDeTexto.length];
+				boolean compCamposVacios = false;
+				String cadena = "";
+				
+				for (int j = 0; j < values.length; j++) { // Almaceno los valorees y compruebo, que no esten vacios
+					values[j] = cuadrosDeTexto[j].getText();
+					cadena += values[j] + ",";
+					if (cuadrosDeTexto[j].getText().equals("")) {
+						compCamposVacios = true;
+					}
+				}
+				
+
+				if (compCamposVacios) { //Si esta vacio muestro un mensaje de error.
+					JOptionPane.showMessageDialog(panelTabbed, "Todos los campos deben estar rellenos");
+				} 
+				else { // Si todos los campos estan rellenos
+					escribirEnfichero(cadena);
+					System.out.println(cadena);
+					model.addRow(values);
+				}
+
+				refrescarPantalla();
+			}
+
+		});
+	}
+
+	private void escribirEnfichero(String cadena) {
+
+		try {
+			FileWriter fw = new FileWriter(ficheroCSV, true);
+			PrintWriter pw = new PrintWriter(fw);
+
+			pw.println(cadena);
+			pw.close();
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+
+	}
+	private void aniadirColumas() {
+		for (int i = 0; i < atributos.size(); i++) {
+			model.addColumn(atributos.get(i));
+		}		
+		
 	}
 	private void aniadirValores() {
 		FileReader fr = null;
@@ -116,7 +218,7 @@ public class Formulario extends JPanel {
 
 			br.close();
 		} catch (Exception e) {
-			// TODO: handle exception
+			System.out.println("error");
 		}
 
 	}
@@ -124,111 +226,6 @@ public class Formulario extends JPanel {
 	private void refrescarPantalla() {
 		this.revalidate();
 		this.repaint();
-
-	}
-
-	private void inicializarComponentes() {
-		GridBagConstraints settings = new GridBagConstraints();
-
-		// Formulario
-		formulario.setBorder(BorderFactory.createTitledBorder("Formulario"));
-		formulario.setBackground(new Color(223, 235, 192));
-
-		panelTabbed.addTab("Formulario", new JScrollPane(formulario));
-
-		// Visor
-		visor.setBorder(BorderFactory.createTitledBorder("Visor"));
-		visor.setBackground(new Color(216, 229, 237));
-		tabla.setBackground(new Color(216, 229, 237));
-
-		visor.add(new JScrollPane(tabla));
-
-		settings.weightx = 1;
-		settings.weighty = 1;
-		settings.fill = GridBagConstraints.BOTH;
-		panelTabbed.addTab("Visor del Fichero", new JScrollPane(visor));
-
-		this.add(panelTabbed);
-		// Añado las etiquetas y los cuadros de Texto
-
-		settings = new GridBagConstraints();
-		settings.insets = new Insets(0, 0, 40, 0);
-		settings.fill = GridBagConstraints.BOTH;
-
-		for (int i = 0; i < atributos.size(); i++) {
-			settings.gridx = 0;
-			settings.gridy = i;
-			settings.weightx = 1;
-			etiquetas[i].setForeground(new Color(136, 61, 29));
-			etiquetas[i].setFont(etiquetas[i].getFont().deriveFont(Font.BOLD, 14f));
-			formulario.add(etiquetas[i], settings);
-
-			settings.gridx = 1;
-			settings.gridy = i;
-			settings.weightx = 1;
-			formulario.add(cuadrosDeTexto[i], settings);
-		}
-
-		// Boton cambiar vista
-
-		settings = new GridBagConstraints();
-		settings.gridx = 0;
-		settings.gridy = 0;
-		settings.gridwidth = 2;
-		settings.weighty = 2;
-		settings.fill = GridBagConstraints.BOTH;
-
-		// Boton almacenar
-
-		settings = new GridBagConstraints();
-
-		settings.gridy = atributos.size();
-		settings.insets = new Insets(50, 0, 0, 0);
-		settings.gridwidth = 2;
-		settings.fill = GridBagConstraints.BOTH;
-		formulario.add(almacenar, settings);
-
-		almacenar.addActionListener(new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent arg0) {
-
-				String values[] = new String[cuadrosDeTexto.length];
-				boolean compCamposVacios = false;
-				String cadena = "";
-				for (int j = 0; j < values.length; j++) {
-					values[j] = cuadrosDeTexto[j].getText();
-					cadena += values[j] + ",";
-					if (cuadrosDeTexto[j].getText().equals("")) {
-						compCamposVacios = true;
-					}
-				}
-
-				if (compCamposVacios) {
-					JOptionPane.showMessageDialog(almacenar, "Todos los campos deben estar rellenos");
-				} else {
-					escribirEnfichero(cadena);
-					System.out.println(cadena);
-					model.addRow(values);
-				}
-
-				refrescarPantalla();
-			}
-
-		});
-	}
-
-	private void escribirEnfichero(String cadena) {
-
-		try {
-			FileWriter fw = new FileWriter(ficheroCSV, true);
-			PrintWriter pw = new PrintWriter(fw);
-
-			pw.println(cadena);
-			pw.close();
-		} catch (Exception e) {
-			// TODO: handle exception
-		}
 
 	}
 }
