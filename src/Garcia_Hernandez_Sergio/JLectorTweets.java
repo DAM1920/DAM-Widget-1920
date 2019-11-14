7037b74e5cea9d811ff312f8097043c904728267
Sergio García <sergiogarcia2893@gmail.com>
2019-11-14 20:04:42 +0100

    Mostrar tweets en la ventana.

--- src/Garcia_Hernandez_Sergio/JLectorTweets.java
+++ src/Garcia_Hernandez_Sergio/JLectorTweets.java
@@ -3,77 +3,123 @@ package Garcia_Hernandez_Sergio;
 import java.awt.Color;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.util.List;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
-import twitter4j.Twitter;
-import twitter4j.TwitterFactory;
+import twitter4j.*;
 import twitter4j.conf.ConfigurationBuilder;
 
-public class JLectorTweets extends JPanel{
+public class JLectorTweets extends JPanel {
 
 	private JTextField textFieldNombreUsuario;
 	private JButton botonMostrarTweets;
-	private JLabel panelTweets;
-	
+	private JTextArea[] panelTweets;
+
 	public JLectorTweets() {
-		super();	// Creación del panel
+		super(); // Creación del panel
 		// Color de fondo
-		setBackground(new Color(150, 250, 250));
-		
+		setBackground(new Color(43, 196, 255));
+
 		// Borde azul oscuro, ancho 3 y sin redondear
-		setBorder(BorderFactory.createLineBorder(new Color(0, 180, 180), 3, false));
-		
+		setBorder(BorderFactory.createLineBorder(new Color(25, 25, 25), 3, false));
+
 		// Inicializamos el JLabel y cambiamos el estilo de letra
-		setLayout(new GridBagLayout());
-		
+		setLayout(new GridLayout(8, 1));
+
 		// Metemos el JLabel
-		GridBagConstraints settings = new GridBagConstraints();
-		settings.gridx = 0;
-		settings.gridy = 0;
-		add(new JLabel("Usuario:    @"), settings);
-		
+		add(new JLabel("Nombre del usuario (sin @)"));
+
 		// Metemos el textFieldNombreUsuario
-		settings = new GridBagConstraints();
-		settings.gridx = 1;
-		settings.gridy = 0;
-		settings.fill = GridBagConstraints.HORIZONTAL;
 		textFieldNombreUsuario = new JTextField();
-		add(textFieldNombreUsuario, settings);
-		
+		add(textFieldNombreUsuario);
+
 		// Añadimos el botón
-		settings = new GridBagConstraints();
-		settings.gridx = 0;
-		settings.gridy = 1;
-		settings.gridwidth = 2;
-		settings.fill = GridBagConstraints.NONE;
 		botonMostrarTweets = new JButton("Mostrar Tweets");
-		add(botonMostrarTweets, settings);
+		add(botonMostrarTweets);
+		
+		panelTweets = new JTextArea[5];
+		
+		for (int i=0; i<5; i++) {
+			panelTweets[i] = new JTextArea();
+			panelTweets[i].setEditable(false);
+			panelTweets[i].setLineWrap(true);
+			panelTweets[i].setBackground(new Color(204, 246, 255));
+			panelTweets[i].setBorder(BorderFactory.createLineBorder(new Color(0, 255, 255)));
+			add(panelTweets[i]);
+		}
+
 		
 		
-		// Añadimos el panel donde se mostrarán los tweets
-		settings = new GridBagConstraints();
-		settings.gridx = 0;
-		settings.gridy = 2;
-		settings.gridwidth = 2;
-		panelTweets = new JLabel("PANEL QUE MUESTRA LOS TWEETS");
-		add(panelTweets, settings);
+		// Inicializamos los listeners
+		inicializarListeners();
 	}
-	
+
 	public void inicializarListeners() {
 		botonMostrarTweets.addActionListener((e) -> {
-			ConfigurationBuilder cb = new ConfigurationBuilder();
-			cb.setDebugEnabled(true)
-			  .setOAuthConsumerKey("bnOzzlRNvqx7a2fTcJZX7pH07")
-			  .setOAuthConsumerSecret("qlj9bOXxXcYY8LbGvX4qKNTlKcTQQRCy9NtPlp7ejB6au3Y99A")
-			  .setOAuthAccessToken("378087758-EVDCoy1FIOqr0ca1VFvjGXj0s9uIA9A7Mql45MBo")
-			  .setOAuthAccessTokenSecret("FzePaRdHx7tZ1aTXAptiGwoJg4akyqd3ijPcm9u4hG9GI");
-			TwitterFactory tf = new TwitterFactory(cb.build());
-			Twitter twitter = tf.getInstance();
-		});
+			Twitter twitter = getObjetoTwitter();
+			Status tweet;
+			
+			try {				
+				String user = textFieldNombreUsuario.getText();
+				
+				List<Status> listaTweets = twitter.getUserTimeline(user); 
+				
+				int tamano = Math.min(listaTweets.size(), 5);
+				
+				// Añadimos el panel donde se mostrarán los tweets
+				//panelTweets = new JTextArea[tamano];
+				
+				for (int i=0; i<tamano; i++) {					
+					// Almacenamos toda la info relativa al tweet
+					tweet = listaTweets.get(i);	
+					
+					//panelTweets[i].add(new JLabel(listaTweets.get(i).getText()));
+					panelTweets[i].setText(tweet.getText());
+					
+					
+//					tweet = "@" + listaTweets.get(i).getUser().getScreenName() + " - "
+//							+ listaTweets.get(i).getText() + "\n";
+//					cadenaTweets += tweet + "\n";
+				}
+				
+			} catch (TwitterException te) {
+				te.printStackTrace();
+				//cadenaTweets = ("No se pudo obtener timeline: " + te.getMessage());
+			} finally {
+				//panelTweets.setText(cadenaTweets);
+			}
+			
+			
+		}); // FIN LISTENER BOTÓN
+	}
+
+	/**
+	 * Obtiene un objeto de la clase Twitter.
+	 * @return Objeto de la clase Twitter
+	 */
+	private Twitter getObjetoTwitter() {
+		ConfigurationBuilder cb = new ConfigurationBuilder();
+		cb.setDebugEnabled(true).setOAuthConsumerKey("bnOzzlRNvqx7a2fTcJZX7pH07")
+				.setOAuthConsumerSecret("qlj9bOXxXcYY8LbGvX4qKNTlKcTQQRCy9NtPlp7ejB6au3Y99A")
+				.setOAuthAccessToken("378087758-EVDCoy1FIOqr0ca1VFvjGXj0s9uIA9A7Mql45MBo")
+				.setOAuthAccessTokenSecret("FzePaRdHx7tZ1aTXAptiGwoJg4akyqd3ijPcm9u4hG9GI");
+		TwitterFactory tf = new TwitterFactory(cb.build());
+		Twitter twitter = tf.getInstance();
+		return twitter;
+	}
+		
+	/**
+	 * Refresca los componentes de la pantalla
+	 */
+	private void refrescarPantalla() {
+		revalidate();
+		repaint();
 	}
 }
