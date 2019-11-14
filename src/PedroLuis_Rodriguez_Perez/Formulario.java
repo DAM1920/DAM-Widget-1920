0dcf025398588763826f768416354b9035c2f991
Pedro <prodriguezp01@informatica.iesvalledeljerteplasencia.es>
2019-11-14 20:17:08 +0100

    Volcado incial del fichero, y escritura en el fichero

--- src/PedroLuis_Rodriguez_Perez/Formulario.java
+++ src/PedroLuis_Rodriguez_Perez/Formulario.java
@@ -3,12 +3,19 @@ package PedroLuis_Rodriguez_Perez;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.GraphicsEnvironment;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.util.ArrayList;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
@@ -21,164 +28,207 @@ import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.table.DefaultTableModel;
 
-public class Formulario extends JPanel {	
+public class Formulario extends JPanel {
+
+	
 	JPanel formulario;
 	JPanel visor;
-	
+
 	JTabbedPane panelTabbed;
-	
+
 	JLabel[] etiquetas;
 	JTextField[] cuadrosDeTexto;
-		
+
 	JTable tabla;
-	DefaultTableModel model = new DefaultTableModel(); 
+	DefaultTableModel model = new DefaultTableModel();
 
 	JButton almacenar;
-	JButton cambiarVista;
-	
-	String [] atributos;
+	File ficheroCSV;
+	ArrayList<String> atributos;
 
-	public Formulario(String[] atributos) {
+	public Formulario(File ficheroCSV) {
+		
 		this.setLayout(new GridLayout());
-		this.atributos = atributos;
 		formulario = new JPanel();
-		formulario.setLayout(new GridBagLayout());		
+		formulario.setLayout(new GridBagLayout());
 		visor = new JPanel();
-		visor.setLayout(new GridLayout(1,1));
-		
+		visor.setLayout(new GridLayout(1, 1));
+		this.ficheroCSV = ficheroCSV;
 		tabla = new JTable(model);
-		for (int i = 0; i < atributos.length; i++) {
-			model.addColumn(atributos[i]);
+		atributos = new ArrayList<String>();
+		volcarAtributos();
+
+		for (int i = 0; i < atributos.size(); i++) {
+			model.addColumn(atributos.get(i));
 		}
-		
-				
-		almacenar= new JButton("Almacenar");
-		cambiarVista= new JButton("Cambiar vista-->");
-		
-		etiquetas = new JLabel[atributos.length];
-		cuadrosDeTexto = new JTextField[atributos.length];
-		
-		for (int i = 0; i < atributos.length; i++) {
-			etiquetas[i] = new JLabel(atributos[i]);
+		aniadirValores();
+
+		almacenar = new JButton("Almacenar");
+
+		etiquetas = new JLabel[atributos.size()];
+		cuadrosDeTexto = new JTextField[atributos.size()];
+
+		for (int i = 0; i < atributos.size(); i++) {
+			etiquetas[i] = new JLabel(atributos.get(i));
 			cuadrosDeTexto[i] = new JTextField();
 		}
-		
+
 		panelTabbed = new JTabbedPane();
-		
+
 		inicializarComponentes();
 	}
-	private void refrescarPantalla() {
-		this.revalidate(); 
-		this.repaint();
-		
+	private void aniadirValores() {
+		FileReader fr = null;
+		BufferedReader br = null;
+		String linea;
+		String[] aux;
+		try {
+			fr = new FileReader(ficheroCSV);
+			br = new BufferedReader(fr);
+
+			aux = br.readLine().split(",");
+			while ((linea = br.readLine()) != null) {
+				aux = linea.split(",");
+				model.addRow(aux);
+			}
+
+			br.close();
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+
 	}
+
+	private void volcarAtributos() {
+		FileReader fr = null;
+		BufferedReader br = null;
+		String linea;
+		String[] aux;
+		try {
+			fr = new FileReader(ficheroCSV);
+			br = new BufferedReader(fr);
+
+			aux = br.readLine().split(",");
+
+			for (int i = 0; i < aux.length; i++) {
+				atributos.add(aux[i]);
+			}
+
+			br.close();
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+
+	}
+
+	private void refrescarPantalla() {
+		this.revalidate();
+		this.repaint();
+
+	}
+
 	private void inicializarComponentes() {
 		GridBagConstraints settings = new GridBagConstraints();
-		
-		
-		
-		// Formulario		
-		formulario.setBorder(BorderFactory.createTitledBorder("Formulario"));
-		formulario.setBackground(new Color(223,235,192));
-		
-		panelTabbed.addTab("Formulario",new JScrollPane(formulario));
 
-		// Visor		
+		// Formulario
+		formulario.setBorder(BorderFactory.createTitledBorder("Formulario"));
+		formulario.setBackground(new Color(223, 235, 192));
+
+		panelTabbed.addTab("Formulario", new JScrollPane(formulario));
+
+		// Visor
 		visor.setBorder(BorderFactory.createTitledBorder("Visor"));
-		visor.setBackground(new Color(216,229,237));
-		tabla.setBackground(new Color(216,229,237));
-		
+		visor.setBackground(new Color(216, 229, 237));
+		tabla.setBackground(new Color(216, 229, 237));
+
 		visor.add(new JScrollPane(tabla));
-		
+
 		settings.weightx = 1;
 		settings.weighty = 1;
 		settings.fill = GridBagConstraints.BOTH;
-		panelTabbed.addTab("Visor del Fichero",new JScrollPane(visor));
-		
-		
+		panelTabbed.addTab("Visor del Fichero", new JScrollPane(visor));
+
 		this.add(panelTabbed);
-		//Añado las etiquetas y los cuadros de Texto
-		
-		settings = new GridBagConstraints();		
+		// Añado las etiquetas y los cuadros de Texto
+
+		settings = new GridBagConstraints();
 		settings.insets = new Insets(0, 0, 40, 0);
 		settings.fill = GridBagConstraints.BOTH;
-		
-		for (int i = 0; i < atributos.length; i++) {
-			settings.gridx=0;
-			settings.gridy=i;
+
+		for (int i = 0; i < atributos.size(); i++) {
+			settings.gridx = 0;
+			settings.gridy = i;
 			settings.weightx = 1;
-			etiquetas[i].setForeground(new Color(136,61,29));
+			etiquetas[i].setForeground(new Color(136, 61, 29));
 			etiquetas[i].setFont(etiquetas[i].getFont().deriveFont(Font.BOLD, 14f));
-			formulario.add(etiquetas[i],settings);
-			
-			settings.gridx=1;
-			settings.gridy=i;
-			settings.weightx = 1;		
-			formulario.add(cuadrosDeTexto[i],settings);
+			formulario.add(etiquetas[i], settings);
+
+			settings.gridx = 1;
+			settings.gridy = i;
+			settings.weightx = 1;
+			formulario.add(cuadrosDeTexto[i], settings);
 		}
-		
-		
-		//Boton cambiar vista
-		
-		settings =new GridBagConstraints();
-		settings.gridx =0;
-		settings.gridy =0;
-		settings.gridwidth=2;
-		settings.weighty=2;
-		settings.fill=GridBagConstraints.BOTH;
-		
-//		this.add(cambiarVista,settings);
-		
-		
-		//Boton almacenar
-		
-		settings =new GridBagConstraints();
-		
-		settings.gridy = atributos.length;
+
+		// Boton cambiar vista
+
+		settings = new GridBagConstraints();
+		settings.gridx = 0;
+		settings.gridy = 0;
+		settings.gridwidth = 2;
+		settings.weighty = 2;
+		settings.fill = GridBagConstraints.BOTH;
+
+		// Boton almacenar
+
+		settings = new GridBagConstraints();
+
+		settings.gridy = atributos.size();
 		settings.insets = new Insets(50, 0, 0, 0);
 		settings.gridwidth = 2;
-		settings.fill = GridBagConstraints.HORIZONTAL;
-		formulario.add(almacenar,settings);
-		
+		settings.fill = GridBagConstraints.BOTH;
+		formulario.add(almacenar, settings);
+
 		almacenar.addActionListener(new ActionListener() {
-			
+
 			@Override
-			public void actionPerformed(ActionEvent arg0) {				
-				
-				String values [] = new String[cuadrosDeTexto.length];
+			public void actionPerformed(ActionEvent arg0) {
+
+				String values[] = new String[cuadrosDeTexto.length];
 				boolean compCamposVacios = false;
+				String cadena = "";
 				for (int j = 0; j < values.length; j++) {
 					values[j] = cuadrosDeTexto[j].getText();
-					if(cuadrosDeTexto[j].getText().equals("")) {
+					cadena += values[j] + ",";
+					if (cuadrosDeTexto[j].getText().equals("")) {
 						compCamposVacios = true;
 					}
 				}
-				
-				if(compCamposVacios) {
+
+				if (compCamposVacios) {
 					JOptionPane.showMessageDialog(almacenar, "Todos los campos deben estar rellenos");
-				}else {
+				} else {
+					escribirEnfichero(cadena);
+					System.out.println(cadena);
 					model.addRow(values);
 				}
-													
+
 				refrescarPantalla();
-			}			
-		});
-		
-		/*cambiarVista.addActionListener(new ActionListener() {
-			
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				if(visor.isVisible()) {
-					visor.setVisible(false);
-					formulario.setVisible(true);
-				}
-				else {
-					visor.setVisible(true);
-					formulario.setVisible(false);
-				}				
 			}
-		});*/
-		
+
+		});
+	}
+
+	private void escribirEnfichero(String cadena) {
+
+		try {
+			FileWriter fw = new FileWriter(ficheroCSV, true);
+			PrintWriter pw = new PrintWriter(fw);
+
+			pw.println(cadena);
+			pw.close();
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
 
 	}
 }
--- src/PedroLuis_Rodriguez_Perez/VentanaPrincipal.java
+++ src/PedroLuis_Rodriguez_Perez/VentanaPrincipal.java
@@ -1,14 +1,15 @@
 package PedroLuis_Rodriguez_Perez;
 
+import java.io.File;
+
 import javax.swing.JFrame;
 
 public class VentanaPrincipal {
 	JFrame marco;
-	Formulario form;
-	String [] atributos = {"Nombre","Apellido","DNI","Ciudad","Edad"};
+	Formulario form;	
 	public VentanaPrincipal() {
 		marco = new JFrame();		
-		form = new Formulario(atributos);
+		form = new Formulario(new File ("txt/empleados.txt"));
 		marco.add(form);
 		marco.setBounds(0, 0, 1000, 1000);
 		marco.setVisible(true);
