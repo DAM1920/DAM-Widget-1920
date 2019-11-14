cc8fb505e636cafa7b3a8263544f9c76dd42f22a
Noah <ngonzalezs01@informatica.iesvalledeljerteplasencia.es>
2019-11-14 20:17:48 +0100

    Mejora de código
    Añadimos el clikc derecho para poder borrar un pixel

--- src/Noah_Gonzalez_Sanz/MiMouse.java
+++ src/Noah_Gonzalez_Sanz/MiMouse.java
@@ -11,124 +11,51 @@ import javax.swing.JColorChooser;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
-public class MiMouse extends JPanel implements MouseListener, ActionListener {
-	JPanel lienzo;
-	JButton botonNegro, botonAzul, botonVerde, botonRojo, botonAmarillo, botonNuevoColor;
-	boolean compBotonNegro=false, compBotonAzul=false, compBotonVerde=false, compBotonRojo=false, compBotonAmarillo=false;
-
-	public MiMouse(JPanel lienzo, JButton botonNegro, JButton botonAzul, JButton botonVerde, JButton botonRojo,
-			JButton botonAmarillo) {
-
-		this.botonNegro = botonNegro;
-		this.botonAzul = botonAzul;
-		this.botonVerde = botonVerde;
-		this.botonRojo = botonRojo;
-		this.botonAmarillo = botonAmarillo;
-		this.lienzo = lienzo;
-
-		botonNegro.addActionListener(new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				compBotonNegro = true;
-			}
-		});
-
-		botonAzul.addActionListener(new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				compBotonAzul = true;
-			}
-		});
-
-		botonVerde.addActionListener(new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				compBotonVerde = true;
-			}
-		});
-
-		botonRojo.addActionListener(new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				compBotonRojo = true;
-			}
-		});
-
-		botonAmarillo.addActionListener(new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent arg0) {
-				compBotonAmarillo = true;
-			}
-		});
+public class MiMouse implements MouseListener{
+	PixelPaint panel;
+	int i,j;
+	
+	public MiMouse(PixelPaint panel, int i, int j) {
+		this.panel = panel;
+		this.i = i;
+		this.j = j;
 	}
 
 	@Override
 	public void mouseClicked(MouseEvent e) {
 		// TODO Auto-generated method stub
-
+		
 	}
-
 	@Override
 	public void mouseEntered(MouseEvent e) {
-		if (SwingUtilities.isLeftMouseButton(e)) {
-			if (compBotonNegro) {
-				lienzo.setBackground(Color.BLACK);
-			}
-			if (compBotonAzul) {
-				lienzo.setBackground(Color.BLUE);
-			}
-			if (compBotonVerde) {
-				lienzo.setBackground(Color.GREEN);
-			}
-			if (compBotonRojo) {
-				lienzo.setBackground(Color.RED);
-			}
-			if (compBotonAmarillo) {
-				lienzo.setBackground(Color.YELLOW);
-			}
+		if(SwingUtilities.isLeftMouseButton(e)) {
+			panel.lienzo[i][j].setBackground(panel.colorPaint);
+		}
+		if(SwingUtilities.isRightMouseButton(e)) {
+			panel.lienzo[i][j].setBackground(Color.WHITE);
 		}
 	}
-
 	@Override
 	public void mouseExited(MouseEvent e) {
 		// TODO Auto-generated method stub
-
+		
 	}
-
 	@Override
 	public void mousePressed(MouseEvent e) {
-
-		if (compBotonNegro) {
-			lienzo.setBackground(Color.BLACK);
+		if(SwingUtilities.isLeftMouseButton(e)) {
+			panel.lienzo[i][j].setBackground(panel.colorPaint);
 		}
-		if (compBotonAzul) {
-			lienzo.setBackground(Color.BLUE);
-			compBotonNegro = false;
+		if(SwingUtilities.isRightMouseButton(e)) {
+			panel.lienzo[i][j].setBackground(Color.WHITE);
 		}
-		if (compBotonVerde) {
-			lienzo.setBackground(Color.GREEN);
-		}
-		if (compBotonRojo) {
-			lienzo.setBackground(Color.RED);
-		}
-		if (compBotonAmarillo) {
-			lienzo.setBackground(Color.YELLOW);
-		}
+		
 	}
 	@Override
 	public void mouseReleased(MouseEvent e) {
 		// TODO Auto-generated method stub
-
+		
 	}
-
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-
-	}
+	
+	
+	
 }
