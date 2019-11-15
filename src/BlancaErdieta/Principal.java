26c97e398ddd95947903b320008f25067ccc8105
Blanca Erdieta <berdietam01@iesvjp.es>
2019-11-15 17:39:55 +0100

    Widget termiando

--- src/BlancaErdieta/Principal.java
+++ src/BlancaErdieta/Principal.java
@@ -1,9 +1,21 @@
 package BlancaErdieta;
 
+import java.awt.EventQueue;
+
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					VentanaPrincipal ventana = new VentanaPrincipal();
+					ventana.inicializar();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 
 	}
 
