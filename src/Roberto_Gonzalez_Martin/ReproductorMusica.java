b526b3669632cc7f80fdefc1832a2275f6aaf8b6
Roberto González Martín <rgonzalezm72@informatica.iesvalledeljerteplasencia.es>
2019-11-19 15:44:51 +0100

    Reproductor de música terminado (a falta de corregir la progresión)

--- src/Roberto_Gonzalez_Martin/ReproductorMusica.java
+++ src/Roberto_Gonzalez_Martin/ReproductorMusica.java
@@ -1,7 +1,9 @@
 package Roberto_Gonzalez_Martin;
+
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
@@ -10,9 +12,11 @@ import java.io.IOException;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.sound.sampled.FloatControl;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JFileChooser;
@@ -21,29 +25,43 @@ import javax.swing.JSlider;
 import javax.swing.JTextField;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
-
-public class ReproductorMusica extends JDialog{
+public class ReproductorMusica extends JDialog {
 	private static final long serialVersionUID = 1L;
+	private static final int MAX_ANCHO_IMAGEN = 15;
+	private static final int MAX_ALTO_IMAGEN = 15;
 	JPanel panelSuperior;
 	JPanel panelCentral;
 	JPanel panelInferior;
 	JTextField nombreCancion;
 	JButton carpeta;
-	JButton reproduccion;
+	JButton reproducir;
+	JButton pausar;
 	JButton avanzar;
 	JButton retroceder;
 	JButton detener;
 	JSlider volumen;
 	JSlider avanceCancion;
 	File cancion;
+	boolean reproduciendo;
+	Long currentFrame;
+	Long maxFrames;
 	AudioInputStream ais;
 	Clip clip;
-	
+	ImageIcon avanzarIcono = new ImageIcon("img/Roberto_Gonzalez_Martin/avanzaricono.png");
+	ImageIcon carpetaIcono = new ImageIcon("img/Roberto_Gonzalez_Martin/carpetaicono.png");
+	ImageIcon detenerIcono = new ImageIcon("img/Roberto_Gonzalez_Martin/detenericono.png");
+	ImageIcon pausarIcono = new ImageIcon("img/Roberto_Gonzalez_Martin/pausaricono.png");
+	ImageIcon reproducirIcono = new ImageIcon("img/Roberto_Gonzalez_Martin/reproduciricono.png");
+	ImageIcon retrocederIcono = new ImageIcon("img/Roberto_Gonzalez_Martin/retrocedericono.png");
+	Image imagen;
+	ImageIcon icono;
+
 	public ReproductorMusica() {
 		super();
 		setModal(true);
-		setBounds(0,0, 500, 300);
+		setBounds(0, 0, 500, 300);
 		anadirElementos();
 		anadirListeners();
 	}
@@ -51,10 +69,10 @@ public class ReproductorMusica extends JDialog{
 	/**
 	 * Método que añade todos los elementos al Dialog. Se codifica como un JFrame
 	 */
-	private void anadirElementos(){
-		this.setLayout(new GridLayout(3,1));
+	private void anadirElementos() {
+		this.setLayout(new GridLayout(3, 1));
 		GridBagConstraints settingObjeto = new GridBagConstraints();
-		
+
 		panelSuperior = new JPanel();
 		panelSuperior.setLayout(new GridBagLayout());
 		panelSuperior.setBorder(BorderFactory.createTitledBorder("Explorador de archivos"));
@@ -65,81 +83,108 @@ public class ReproductorMusica extends JDialog{
 		settingObjeto.weightx = 9;
 		settingObjeto.fill = GridBagConstraints.HORIZONTAL;
 		settingObjeto.anchor = GridBagConstraints.NORTH;
-		panelSuperior.add(nombreCancion,settingObjeto);
-		
-		
+		panelSuperior.add(nombreCancion, settingObjeto);
+
 		settingObjeto = new GridBagConstraints();
-		carpeta = new JButton("Canción");
+		carpeta = new JButton();
+		imagen = carpetaIcono.getImage();
+		icono = new ImageIcon(imagen.getScaledInstance(MAX_ANCHO_IMAGEN, MAX_ALTO_IMAGEN, Image.SCALE_SMOOTH));
+		carpeta.setIcon(icono);
 		settingObjeto.gridx = 1;
 		settingObjeto.gridy = 0;
 		settingObjeto.weightx = 1;
 		settingObjeto.fill = GridBagConstraints.HORIZONTAL;
 		settingObjeto.anchor = GridBagConstraints.NORTH;
-		panelSuperior.add(carpeta,settingObjeto);
+		panelSuperior.add(carpeta, settingObjeto);
 		this.add(panelSuperior);
-		
+
 		panelCentral = new JPanel();
-		panelCentral.setLayout(new GridLayout(1,1));
+		panelCentral.setLayout(new GridLayout(1, 1));
 		panelCentral.setBorder(BorderFactory.createTitledBorder("Progreso de la canción"));
-		avanceCancion = new JSlider(JSlider.HORIZONTAL,0,100,0);
+		avanceCancion = new JSlider(JSlider.HORIZONTAL, 0, 100, 0);
 		panelCentral.add(avanceCancion);
 		this.add(panelCentral);
-		
+
 		panelInferior = new JPanel();
 		panelInferior.setLayout(new GridBagLayout());
 		panelInferior.setBorder(BorderFactory.createTitledBorder("Controles de la canción"));
 		settingObjeto = new GridBagConstraints();
-		retroceder = new JButton("<<");
+		retroceder = new JButton();
+		imagen = retrocederIcono.getImage();
+		icono = new ImageIcon(imagen.getScaledInstance(MAX_ANCHO_IMAGEN, MAX_ALTO_IMAGEN, Image.SCALE_SMOOTH));
+		retroceder.setIcon(icono);
 		settingObjeto.gridx = 0;
 		settingObjeto.gridy = 0;
 		settingObjeto.weightx = 1;
 		settingObjeto.gridwidth = 1;
-		panelInferior.add(retroceder,settingObjeto);
-		
+		panelInferior.add(retroceder, settingObjeto);
+
 		settingObjeto = new GridBagConstraints();
-		reproduccion = new JButton("Play");
+		reproducir = new JButton();
+		imagen = reproducirIcono.getImage();
+		icono = new ImageIcon(imagen.getScaledInstance(MAX_ANCHO_IMAGEN, MAX_ALTO_IMAGEN, Image.SCALE_SMOOTH));
+		reproducir.setIcon(icono);
 		settingObjeto.gridx = 1;
 		settingObjeto.gridy = 0;
 		settingObjeto.weightx = 1;
 		settingObjeto.gridwidth = 1;
-		panelInferior.add(reproduccion,settingObjeto);
-		
+		panelInferior.add(reproducir, settingObjeto);
+
 		settingObjeto = new GridBagConstraints();
-		detener = new JButton("Stop");
+		pausar = new JButton();
+		imagen = pausarIcono.getImage();
+		icono = new ImageIcon(imagen.getScaledInstance(MAX_ANCHO_IMAGEN, MAX_ALTO_IMAGEN, Image.SCALE_SMOOTH));
+		pausar.setIcon(icono);
 		settingObjeto.gridx = 2;
 		settingObjeto.gridy = 0;
 		settingObjeto.weightx = 1;
 		settingObjeto.gridwidth = 1;
-		panelInferior.add(detener,settingObjeto);
-		
+		panelInferior.add(pausar, settingObjeto);
+
 		settingObjeto = new GridBagConstraints();
-		avanzar = new JButton(">>");
+		detener = new JButton();
+		imagen = detenerIcono.getImage();
+		icono = new ImageIcon(imagen.getScaledInstance(MAX_ANCHO_IMAGEN, MAX_ALTO_IMAGEN, Image.SCALE_SMOOTH));
+		detener.setIcon(icono);
 		settingObjeto.gridx = 3;
 		settingObjeto.gridy = 0;
 		settingObjeto.weightx = 1;
 		settingObjeto.gridwidth = 1;
-		panelInferior.add(avanzar,settingObjeto);
-		
+		panelInferior.add(detener, settingObjeto);
+
 		settingObjeto = new GridBagConstraints();
-		volumen = new JSlider(JSlider.HORIZONTAL,0,100,50);
-		volumen.setBorder(BorderFactory.createTitledBorder("Volumen"));
+		avanzar = new JButton();
+		imagen = avanzarIcono.getImage();
+		icono = new ImageIcon(imagen.getScaledInstance(MAX_ANCHO_IMAGEN, MAX_ALTO_IMAGEN, Image.SCALE_SMOOTH));
+		avanzar.setIcon(icono);
 		settingObjeto.gridx = 4;
+		settingObjeto.gridy = 0;
+		settingObjeto.weightx = 1;
+		settingObjeto.gridwidth = 1;
+		panelInferior.add(avanzar, settingObjeto);
+
+		settingObjeto = new GridBagConstraints();
+		volumen = new JSlider(JSlider.HORIZONTAL, 0, 100, 100);
+		volumen.setBorder(BorderFactory.createTitledBorder("Volumen"));
+		settingObjeto.gridx = 5;
 		settingObjeto.gridy = 0;
 		settingObjeto.weightx = 4;
 		settingObjeto.gridwidth = 4;
 		settingObjeto.fill = GridBagConstraints.HORIZONTAL;
-		panelInferior.add(volumen,settingObjeto);
+		panelInferior.add(volumen, settingObjeto);
 		this.add(panelInferior);
 	}
-	
+
 	/**
 	 * Método que añade todos los listeners al Dialog
 	 */
-	private void anadirListeners(){
+	private void anadirListeners() {
 		carpeta.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				JFileChooser fileChooser = new JFileChooser();
+				FileNameExtensionFilter fileFilter = new FileNameExtensionFilter("Archivos de audio", "wav");
+				fileChooser.setFileFilter(fileFilter);
 				fileChooser.showOpenDialog(fileChooser);
 				cancion = fileChooser.getSelectedFile();
 				nombreCancion.setText(cancion.getName());
@@ -147,54 +192,94 @@ public class ReproductorMusica extends JDialog{
 					ais = AudioSystem.getAudioInputStream(cancion.getAbsoluteFile());
 					clip = AudioSystem.getClip();
 					clip.open(ais);
-				} catch (UnsupportedAudioFileException e1) {
-					e1.printStackTrace();
-				} catch (IOException e1) {
-					e1.printStackTrace();
-				} catch (LineUnavailableException e1) {
-					e1.printStackTrace();
-				}
-			}
-		});
-		reproduccion.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				float duracion = clip.getMicrosecondLength();
-				float avance = clip.getMicrosecondPosition();
-				float posicion = (100*avance)/duracion;
-				if(clip.isRunning()) {
-					clip.stop();
-				} else {
 					clip.start();
+					reproduciendo = true;
+					currentFrame = 0l;
+					maxFrames = clip.getMicrosecondLength();
+					avanceCancion.setMinimum(0);
+					avanceCancion.setMaximum((int) (long) maxFrames);
+				} catch (UnsupportedAudioFileException | IOException | LineUnavailableException e1) {
+					e1.printStackTrace();
 				}
-				while(clip.isRunning()) {
-					avanceCancion.setValue((int)posicion);
-				}
-			}
-		});
-		detener.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				clip.stop();
 			}
 		});
 		retroceder.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				
+				saltoCancion(clip.getMicrosecondPosition()-10000000);
+				avanceCancion.setValue((int)(long)currentFrame);
+			}
+		});
+		reproducir.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (!reproduciendo) {
+					clip.close();
+					resetAudioStream();
+					clip.setMicrosecondPosition(currentFrame);
+					clip.start();
+				}
+			}
+		});
+		pausar.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				currentFrame = clip.getMicrosecondPosition();
+				avanceCancion.setValue((int)(long)currentFrame);
+				clip.stop();
+				reproduciendo = false;
+			}
+		});
+		detener.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				currentFrame = 0l;
+				avanceCancion.setValue((int)(long)currentFrame);
+				clip.stop();
+				reproduciendo = false;
 			}
 		});
 		avanzar.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				
+				saltoCancion(clip.getMicrosecondPosition()+10000000);
+				avanceCancion.setValue((int)(long)currentFrame);
+			}
+		});
+		avanceCancion.addChangeListener(new ChangeListener() {
+			@Override
+			public void stateChanged(ChangeEvent e) {
+				saltoCancion((int) (long) avanceCancion.getValue());
 			}
 		});
 		volumen.addChangeListener(new ChangeListener() {
 			@Override
 			public void stateChanged(ChangeEvent e) {
-				
+				FloatControl controlVolumen = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+				volumen.setMinimum((int) controlVolumen.getMinimum());
+				volumen.setMaximum((int) controlVolumen.getMaximum());
+				controlVolumen.setValue((float) volumen.getValue());
 			}
 		});
+	}
+
+	public void saltoCancion(long saltoCancion) {
+		if (saltoCancion > 0 && saltoCancion < clip.getMicrosecondLength()) {
+			clip.stop();
+			clip.close();
+			resetAudioStream();
+			currentFrame = saltoCancion;
+			clip.setMicrosecondPosition(saltoCancion);
+			clip.start();
+		}
+	}
+
+	public void resetAudioStream() {
+		try {
+			ais = AudioSystem.getAudioInputStream(cancion.getAbsoluteFile());
+			clip.open(ais);
+		} catch (UnsupportedAudioFileException | IOException | LineUnavailableException e1) {
+			e1.printStackTrace();
+		}
 	}
 }
